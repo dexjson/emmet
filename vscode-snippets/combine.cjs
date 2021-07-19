d8a7ae736aa950ee9ffcdba35c45be0893bb168b
dexjson <50504842+dexjson@users.noreply.github.com>
2021-07-19 13:14:17 -0400

    Create combine.cjs

---
+++ vscode-snippets/combine.cjs
@@ -0,0 +1,30 @@
+const fs = require('fs')
+
+const css = require('../snippets/css.json')
+const html = require('../snippets/html.json')
+const pug = require('../snippets/pug.json')
+const variables = require('../snippets/variables.json')
+const xsl = require('../snippets/xsl.json')
+
+const combined = {
+    'css': {
+        'snippets': css
+    },
+    'html': {
+        'snippets': html
+    },
+    'pug': {
+        'snippets': pug
+    },
+    'variables': {
+        'snippets': variables
+    },
+    'xsl': {
+        'snippets': xsl
+    }
+}
+
+fs.writeFile('./vscode-snippets/emmet/default/settings.json', JSON.stringify(combined, null, 2), err => {
+    if (err) throw err
+    console.log('combined json files as "settings.json"')
+})
